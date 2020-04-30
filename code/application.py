5541a15fabda5aadb45009bed0a568c0bda7657f
Penguin2Squishy <jenf@rpi.edu>
2020-04-30 16:38:59 -0400

    Added compare_dates to application.py

--- code/application.py
+++ code/application.py
@@ -3,6 +3,18 @@ from code.database import SqlOperator
 
 connection_string = "host='localhost' dbname='dbms_final_project' user='dbms_project_user' password='dbms_password'"
 
+
+# checks if date1 is more recent than date2
+def __compare_date(date1, date2):
+    date1_nums = [int(date1[0] + date1[1]), int(date1[3] + date1[4]), int(date1[6] + date1[7] + date1[8] + date1[9]),
+                  int(date1[11] + date1[12]), int(date1[14] + date1[15]), int(date1[17] + date1[18])]
+    date2_nums = [int(date2[0] + date2[1]), int(date2[3] + date2[4]), int(date2[6] + date2[7] + date2[8] + date2[9]),
+                  int(date2[11] + date2[12]), int(date2[14] + date2[15]), int(date2[17] + date2[18])]
+    return date1_nums[2] > date2_nums[2] or date1_nums[0] > date2_nums[0] or date1_nums[1] > date2_nums[1] or \
+        date1[20] < date2[20] or date1_nums[3] > date2_nums[3] or date1_nums[4] > date2_nums[4] or \
+           date1_nums[5] > date2_nums[5]
+
+
 def __check_input(string, type):
     if type == "float":
         if isinstance(string, (float, int)) == False:
@@ -46,6 +58,7 @@ def main():
             lower_long = float(input())
             print("Input Upper Longitude:")
             upper_long = float(input())
+
             # check input
             if __check_input(lower_lat, "float") == False or __check_input(upper_lat, "float") == False or \
                     __check_input(lower_long, "float") == False or __check_input(upper_long, "float") == False or \
@@ -53,6 +66,7 @@ def main():
                 correct_input = False
             elif lower_lat >= upper_lat or lower_long >= upper_long:
                 print("ERROR: Bounds are mismatched")
+
             # execute map op
             if correct_input:
                 # execute code
@@ -70,12 +84,14 @@ def main():
             lower_long = float(input())
             print("Input Upper Longitude:")
             upper_long = float(input())
+
             # check input
             if __check_input(lower_lat, "float") == False or __check_input(upper_lat, "float") == False or \
                     __check_input(lower_long, "float") == False or __check_input(upper_long, "float") == False:
                 correct_input = False
             elif lower_lat >= upper_lat or lower_long >= upper_long:
                 print("ERROR: Bounds are mismatched")
+
             # execute map op
             if correct_input:
                 # execute code and get both relations range
@@ -90,10 +106,15 @@ def main():
             lower_date = input()
             print("input second date(recent):")
             upper_date = input()
+
             # check input
             if __check_input(lower_date, "date") == False or __check_input(upper_date, "float") == False or \
                     __check_input(relation, "relation"):
                 correct_input = False
+            if __compare_date(upper_date, lower_date) == False:
+                print("ERROR: Date bounds have negative distance")
+                correct_input = False
+
             # execute map op
             if correct_input:
                 # do the same as range coordinate one
@@ -101,13 +122,19 @@ def main():
         elif cmd == "\\range date union":
             correct_input = True
             # get input
+            print("Input Date as MM/DD/YYYY HH:MI:SS AM.")
             print("Input first date(oldest):")
             lower_date = input()
             print("input second date(recent):")
             upper_date = input()
+
             # check input
             if __check_input(lower_date, "date") == False or __check_input(upper_date, "float") == False:
                 correct_input = False
+            if __compare_date(upper_date, lower_date) == False:
+                print("ERROR: Date bounds have negative distance")
+                correct_input = False
+
             # execute map op
             if correct_input:
                 # execute range date on both relations and extend one of them
@@ -119,9 +146,11 @@ def main():
             latitude = input()
             print("Input Longitude:")
             longitude = input()
+
             # check input
             if __check_input(latitude, "float") == False or __check_input(longitude, "float") == False:
                 correct_input = False
+
             # execute map op
             if correct_input:
                 # execute input on both maps
